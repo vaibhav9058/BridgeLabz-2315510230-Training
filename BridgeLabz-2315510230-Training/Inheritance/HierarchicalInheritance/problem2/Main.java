1e27dcca34fd5c60cf16ad4be17c5e7f922aa76c
vaibhav9058 <vaibhavkubc@gmail.com>
2025-09-17 09:43:25 +0530

    Add files via upload

---
+++ BridgeLabz-2315510230-Training/Inheritance/HierarchicalInheritance/problem2/Main.java
@@ -0,0 +1,57 @@
+
+// Hierarchical Inheritance - Problem 2: School System with Roles
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Teacher extends Person {
+    String subject;
+
+    Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+
+    void displayRole() {
+        System.out.println("Teacher of " + subject);
+    }
+}
+
+class Student extends Person {
+    int grade;
+
+    Student(String name, int age, int grade) {
+        super(name, age);
+        this.grade = grade;
+    }
+
+    void displayRole() {
+        System.out.println("Student in grade " + grade);
+    }
+}
+
+class Staff extends Person {
+    String position;
+
+    Staff(String name, int age, String position) {
+        super(name, age);
+        this.position = position;
+    }
+
+    void displayRole() {
+        System.out.println("Staff: " + position);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Teacher t = new Teacher("Alice", 35, "Math");
+        t.displayRole();
+    }
+}
